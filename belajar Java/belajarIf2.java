46973d7115d8dafa7671b94f94929f77a356cf7b
TeukuReyn <91148295+Mystereyn@users.noreply.github.com>
2022-02-15 20:05:10 +0700

    Create belajarIf2.java

---
+++ belajar Java/belajarIf2.java
@@ -0,0 +1,14 @@
+public class belajarIf2 { //harus sesuai nama domain file.
+    public static void main(String[] args) {
+        int  A = 92; //variabel
+        int  B = 89; //variabel
+        if (A > B) { //pernyataan variabel, tanda > dalah lbih besar
+
+            //kesalahan pada syntax "sistem.", jika S huruf kecil
+            //Maka case sensitif pada java akan aktif.
+            System.out.println("A lebih besar dari B");    
+        }
+
+    }
+    
+}
