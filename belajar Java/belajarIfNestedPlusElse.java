40c72e486bed70746e531f44dca4d6904235724e
TeukuReyn <91148295+Mystereyn@users.noreply.github.com>
2022-02-17 20:17:16 +0700

    Update and rename belajarIfElse.java to belajarIfNestedPlusElse.java

---
+++ belajar Java/belajarIfNestedPlusElse.java
@@ -0,0 +1,16 @@
+public class belajarIfNestedPlusElse {
+    public static void main(String[] args) {
+        int nilaiKKM = 96; //Tak bisa di spasi
+
+        if (nilaiKKM>96) {
+            System.out.println("Good Teuku Keep it up!");
+        }else if(nilaiKKM>96) {
+            System.out.println("Very awesome Teuku!");
+        }else if(nilaiKKM>96) {
+            System.out.println("Keep it up alive!");
+        }else {
+            System.out.println("Done");
+        }
+    }
+    
+}
