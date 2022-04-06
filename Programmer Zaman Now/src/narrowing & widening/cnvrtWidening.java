aee211785ccc68c3082ea3422eb587d07a38d1b9
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-06 23:09:19 +0700

    Add files via upload

---
+++ Programmer Zaman Now/src/narrowing & widening/cnvrtWidening.java
@@ -0,0 +1,13 @@
+public class convertTipeDataNumber {
+    public static void main(String[] args) {
+        //contoh ini adalah jika Widening Casting (Auto)
+        //Widening Casting (auto) adalah jika connvert dari berikut ini secara otomatis
+        //Byte -> Short -> int -> Long -> float -> double
+        //Jika long ke ke integer tidak bisa, karna kapasitas maksimal integer tidak muat
+
+        byte thisByte = 10;
+        short thisShort = thisByte;
+
+
+    }
+}
