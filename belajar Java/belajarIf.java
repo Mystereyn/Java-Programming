2ec5421807e538c2eca788ee5f1af55130b2e07a
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-06 22:27:00 +0700

    Delete belajar Java directory

--- belajar Java/belajarIf.java
+++
@@ -1,8 +0,0 @@
-public class belajarIf { //Sesuai nama file pada domain file.
-    public static void main(String[] args) { //syntax wajib pada java, sama halnya seperti clearscreen pada pascal 
-        int berat = 92; // ini adalah variabel yang akan di eksekusi oleh java
-        if (berat>90) { // ini adalah pernyataan if 
-            System.out.println("Berat lebih besar daripada 90");
-        }
-    }
-}
