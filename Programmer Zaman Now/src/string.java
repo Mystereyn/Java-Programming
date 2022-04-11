08d5e87e5e81503c9d27e8bbed7b934f4c03f63e
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-11 09:31:58 +0700

    Add files via upload

---
+++ Programmer Zaman Now/src/string.java
@@ -0,0 +1,14 @@
+public class string {
+
+    public static void main(String[] args) {
+        
+        String awalNama = "Teuku Reynaldi";
+        String namaBelakang = "Putra Irwansyah";
+
+        String fullName = awalNama + " " + namaBelakang;
+
+        System.out.println(awalNama);
+        System.out.println(namaBelakang);
+        System.out.println(fullName);
+    }
+}
