aa3aef3741f14e75b18bd51b94a75c32883d38bd
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-12 21:14:22 +0700

    Add files via upload

---
+++ Programmer Zaman Now/src/manipulasiString/lowerCase.java
@@ -0,0 +1,14 @@
+package manipulasiString;
+
+public class lowerCase {
+
+    public static void main(String[] args) {
+        
+        String salam = "TEUKU REYNALDI PUTRA";
+
+        System.out.println(salam.toLowerCase());
+    }
+
+    
+    
+}
---
+++ Programmer Zaman Now/src/manipulasiString/manipulateUpper.java
@@ -0,0 +1,12 @@
+package manipulasiString;
+public class manipulateUpper {
+
+    public static void main(String[] args) {
+        
+        String salam = "Assalamualaikum";
+
+        System.out.println(salam);
+        System.out.println(salam.toUpperCase());
+    }
+    
+}
