a1cb760f8229726b3ba1892e39cc24ee08315835
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-17 21:44:36 +0700

    Tipe Data non Primitive.
    Primitive = Tipe data bawaan pada tiap bahasa pemrograman

---
+++ Programmer Zaman Now/Ctatan Tipe Data non primitiv/tipeBukanPrimitif.java
@@ -0,0 +1,30 @@
+public class tipeBukanPrimitif {
+
+    public static void main(String[] args) {
+        
+
+        /*jika tidak ingin mengisi nilai, maka insert saja null.
+        
+        secara default, nilai dari semua TipeNonPrimitive adalah null(kosong)
+
+        String bukan lah Primitive maupun non-Primitive. Melainkan adalah tipe Data
+        Objek.
+        
+        */
+
+        Character thisChar = null;
+        Float thisFloat= null;
+        Integer thisInteger = null;
+        Boolean thisBoolean = null;
+        Byte thisByte = null;
+
+        //menampilkan variabel
+        System.out.println(thisByte);
+        System.out.println(thisFloat);
+        System.out.println(thisChar);
+        System.out.println(thisBoolean);
+        System.out.println(thisInteger);
+        
+    }
+    
+}
