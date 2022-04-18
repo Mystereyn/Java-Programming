8cc06a678ff67c39fa98623961830b6dd4bcc18c
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-18 07:46:46 +0700

    perubahan manipulasi lower

---
+++ Programmer Zaman Now/src/manipulasiString/lowerCase.java
@@ -0,0 +1,15 @@
+package manipulasiString;
+
+public class lowerCase {
+
+    public static void main(String[] args) {
+        
+        String salam = "TEUKU REYNALDI PUTRA";
+
+        System.out.println(salam);
+        System.out.println(salam.toLowerCase());
+    }
+
+    
+    
+}
