a8ad84c197cf65b5735d91ff9c64b14125886824
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-21 20:18:50 +0700

    Add files via upload

--- Programmer Zaman Now/Ctatan Tipe Data non primitiv/tipeBukanPrimitif.java
+++ Programmer Zaman Now/Ctatan Tipe Data non primitiv/tipeBukanPrimitif.java
@@ -1,3 +1,4 @@
+package nonPrimitive;
 public class tipeBukanPrimitif {
 
     public static void main(String[] args) {
@@ -12,19 +13,24 @@ public class tipeBukanPrimitif {
         
         */
 
-        Character thisChar = null;
-        Float thisFloat= null;
-        Integer thisInteger = null;
-        Boolean thisBoolean = null;
+    
+        Float thisFloat= 20.2f;
+        Integer thisInteger = 1000;
         Byte thisByte = null;
+
+        //jika Byte diganti dengan value 100
+
+        thisByte = 100;
 
         //menampilkan variabel
         System.out.println(thisByte);
         System.out.println(thisFloat);
-        System.out.println(thisChar);
-        System.out.println(thisBoolean);
+      
+      
         System.out.println(thisInteger);
         
+        System.out.println("Semua Hasil tipe datanya = " + thisFloat*thisInteger*thisByte);
+
     }
     
 }
