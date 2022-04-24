6eb2bcaeed705e26d57c93fc457d349092c65320
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-24 15:28:29 +0700

    Update cobaArray.java

--- Programmer Zaman Now/src/belajarArray/cobaArray.java
+++ Programmer Zaman Now/src/belajarArray/cobaArray.java
@@ -17,7 +17,23 @@ public class cobaArray {
         arrayString[1] = "Reynaldi"; //data ke-2
         arrayString[2] = "Putra"; //data ke-3
 
-        jika ingin mengubah isian data Array
+        // jika ingin mengubah isian data Array
+        
+        
+        ------------------------------------------------------------
+        
+        String[] arrayString;
+        arrayString = new String[3]; //inisalisasi variable
+
+        arrayString[0] = "Teuku";
+        arrayString[1] = "Reynaldi";
+        arrayString[2] = "Putra";
+
+        System.out.println(arrayString[0]);
+        System.out.println(arrayString[1]);
+        System.out.println(arrayString[2]);
+
+        // String[] arrayString2 = new String[3]; //jika ingin langsung
 
 
          */
