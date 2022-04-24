17345b0eea316dae4986f5388c6709190fbe5cc6
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-24 14:25:53 +0700

    Update cobaArray.java

--- Programmer Zaman Now/src/belajarArray/cobaArray.java
+++ Programmer Zaman Now/src/belajarArray/cobaArray.java
@@ -3,6 +3,24 @@ package belajarArray;
 public class cobaArray {
 
     public static void main(String[] args) {
+        
+            /* Contoh array Java
+
+        String[] arrayString; //variable
+
+        arrayString = new String[3]; //inisialisasi array dengan kata
+        kuncu "new", berjumlah 3 data array
+
+        -----------------------------------------------------------
+
+        arrayString[0] = "Teuku"; //data ke-1
+        arrayString[1] = "Reynaldi"; //data ke-2
+        arrayString[2] = "Putra"; //data ke-3
+
+        jika ingin mengubah isian data Array
+
+
+         */
 
         
     }
