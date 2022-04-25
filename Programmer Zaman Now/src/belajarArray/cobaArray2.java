470b6beb54a50101a9b487f64193efcbb54d4862
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-25 12:28:07 +0700

    Add files via upload
    char[] untuk mengeja kalimat depan
    String[] untuk nama lengkap

---
+++ Programmer Zaman Now/src/belajarArray/cobaArray2.java
@@ -0,0 +1,25 @@
+package belajarAray;
+
+public class cobaArray2 {
+
+    public static void main(String[] args) {
+
+        char[] ejaHuruf = {'T', 'E', 'U', 'K', 'U'}; //variable array
+        String[] namaLengkap = {"Teuku", "Reynaldi", "Putra", "Irwansyah"};
+
+        //untuk char[]
+        System.out.println(ejaHuruf[0]);
+        System.out.println(ejaHuruf[1]);
+        System.out.println(ejaHuruf[2]);
+        System.out.println(ejaHuruf[3]);
+        System.out.println(ejaHuruf[4]);
+        System.out.println();
+
+        //untuk String[]
+        System.out.println(namaLengkap[0]);
+        System.out.println(namaLengkap[1]);
+        System.out.println(namaLengkap[2]);
+        System.out.println(namaLengkap[3]);
+
+    }
+}
