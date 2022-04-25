2e4b4a82804d97f0f2d6ac097fbbc2014c4ac9db
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-25 18:38:09 +0700

    looping array java with String[]

---
+++ Programmer Zaman Now/src/belajarArray/loopingArray.java
@@ -0,0 +1,13 @@
+package belajarAray;
+
+public class loopingArray {
+    public static void main(String[] args) {
+
+        String[] namaLengkap = {"Teuku", "Reynaldi", "Putra", "Irwansyah"};
+
+        for (int i = 0; i < namaLengkap.length; i++) {
+
+            System.out.println(namaLengkap[i]);
+        }
+    }
+}
