1093c0cb321a5cb0b8f908941c43dc06c241458f
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-04-30 19:00:22 +0700

    Add files via upload

--- Programmer Zaman Now/src/belajarArray/array2d.java
+++ Programmer Zaman Now/src/belajarArray/array2d.java
@@ -5,22 +5,23 @@ public class array2d {
 
         //array in array atau array2dimensi
 
-        String[][] fullName = {
-                {"Teuku","Reynaldi"}, //array pertama
-                {"Putra","Irwansyah"}, //array kedua
-        };
+        String[][] namaLengkap = { {"Teuku","Reynaldi"}, {"Putra","Irwansyah"}  };
 
-        System.out.println(fullName[0][0]); //Output Teuku
-        System.out.println(fullName[0][1]); //Output Reynaldi
-        System.out.println();
-        System.out.println(fullName.length); //output 2
-        System.out.println();
+        System.out.println(namaLengkap[0][0]); //Output Teuku
+        System.out.println(namaLengkap[0][1]); //Output Reynald
+        System.out.println(namaLengkap.length); //output 2
+        System.out.println("\n");
+
         //array kedua
-        System.out.println(fullName[1][0]); //output putra
-        System.out.println(fullName[1][1]); //output irwansyah
+        System.out.println(namaLengkap[1][0]); //output putra
+        System.out.println(namaLengkap[1][1]); //output irwansyah
+        System.out.println("\n");
 
-        for (int i=0; i < fullName.length; i++) {
-            System.out.println("Semua data array 1 dan 2  = " +  fullName[i][i]);
+        //jika pake nested for
+        for (int i=0; i < 2; i++) { //untuk menentukan elemen array1
+            for (int j = 0; j < 2; j++) { //untuk menentukan elemen array2
+                System.out.println(namaLengkap[i][j]);
+            }
         }
     }
 }
