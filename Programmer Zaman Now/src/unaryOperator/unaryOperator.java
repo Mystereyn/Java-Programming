28186d146546ab5ed0a08b0ee4e2880da3421b43
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-05-02 19:29:19 +0700

    Update unaryOperator.java

--- Programmer Zaman Now/src/unaryOperator/unaryOperator.java
+++ Programmer Zaman Now/src/unaryOperator/unaryOperator.java
@@ -3,6 +3,11 @@ package unaryOperator;
 public class unaryOperator {
     public static void main(String[] args) {
 
-        
+        int d = 100;
+        d++; //ini 10 ++ (bertambah satu)
+        System.out.println(d);
+
+        d--; //outpunyt adalah 100 -- (berkurang satu)
+        System.out.println(d);
     }
 }
