9d1509c65c77b7b4f8d5908817e527da4a43cd84
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-05-02 19:10:19 +0700

    Update augmentedAssignments.java

--- Programmer Zaman Now/src/AugmentedAssignments/augmentedAssignments.java
+++ Programmer Zaman Now/src/AugmentedAssignments/augmentedAssignments.java
@@ -1,4 +1,21 @@
 package AugmentedAssignments;
 
 public class augmentedAssignments {
+    public static void main(String[] args) {
+
+        int C = 100;
+
+        C += 10;
+        System.out.println(C);
+
+        C -= 10; //output adalah 100, karena diatas sudah ditambah 110, maka dikurangin -10 adalah 100
+        System.out.println(C);
+
+        C *= 10;
+        System.out.println(C);
+
+        C /= 10; //output juga 100, karena diatas sudah dikalikan 1000, maka dibagi /10 menjadi 100 lagi.
+        System.out.println(C);
+
+    }
 }
