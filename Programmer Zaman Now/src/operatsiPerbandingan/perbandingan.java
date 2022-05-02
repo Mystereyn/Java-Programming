26f6dffcd3cd380388178e7f679dca41ca9c1693
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-05-02 19:46:57 +0700

    Update perbandingan.java

--- Programmer Zaman Now/src/operatsiPerbandingan/perbandingan.java
+++ Programmer Zaman Now/src/operatsiPerbandingan/perbandingan.java
@@ -2,5 +2,17 @@ package operatsiPerbandingan;
 
 public class perbandingan {
 
-    
+    public static void main(String[] args) {
+
+        int nilai1 = 100;
+        int nilai2 = 100;
+
+        System.out.println(nilai1 > nilai2);
+        System.out.println(nilai1 < nilai2);
+        System.out.println(nilai1 >= nilai2);
+        System.out.println(nilai1 <= nilai2);
+        System.out.println(nilai1 == nilai2);
+        System.out.println(nilai1 != nilai2);
+        
+    }
 }
