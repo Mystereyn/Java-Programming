678fe2a74fa8c9e37cfeaf5bbc7719fe7e2a4133
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-05-12 19:44:34 +0700

    Add files via upload

---
+++ Programmer Zaman Now/src/Expressions/ekspresi.java
@@ -0,0 +1,13 @@
+package Expressions;
+
+public class ekspresi {
+
+    public static void main(String[] args) {
+
+        //expressions atau ekspresi adalah satu single value
+        int value;
+        value = 100;
+
+        System.out.println(value = 100); // >>>  statement = menyatakan pernyataan
+    }
+}
