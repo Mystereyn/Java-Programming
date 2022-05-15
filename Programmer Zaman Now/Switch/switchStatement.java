8931e9faa2a735d60745bd3df584671aaa6b60c4
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-05-15 10:46:44 +0700

    SwitchState

---
+++ Programmer Zaman Now/Switch/switchStatement.java
@@ -0,0 +1,27 @@
+package Switch;
+
+public class switchStatement {
+    public static void main(String[] args) {
+
+        String nilai = "";
+
+        switch (nilai) {
+
+            case "A":
+                System.out.println("anda lulus");
+                break;
+            case "B":
+                System.out.println("Anda cukup baik untuk lulus");
+                break;
+            case "C":
+                System.out.println("Anda cukup lulus");
+                break;
+            case "D":
+                System.out.println("Anda gagal");
+                break;
+            default:
+                System.out.println("Anda lebih baik giat belajar!");
+
+            };
+        }
+    }
