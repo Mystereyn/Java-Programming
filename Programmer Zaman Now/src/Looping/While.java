85975249384dee8457b08cb55a70d93e112c1800
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-05-16 20:16:27 +0700

    while java within objek string

--- Programmer Zaman Now/src/Looping/While.java
+++ Programmer Zaman Now/src/Looping/While.java
@@ -3,10 +3,27 @@ package Looping;
 public class While {
     public static void main(String[] args) {
 
-        int i = 0;
-        while(i <= 10) {
-            System.out.println(i);
-            i++;
+        {
+            int i = 0;
+
+            while (i <= 10) {
+                System.out.println(i);
+                i++;
+            }
+            System.out.println();
         }
+
+        //jika objek String dimasukkan ke WHile looping.
+        {
+            String nama = "Teuku Reynaldi Putra";
+            int j = 0;
+
+            while (j <= 20) {
+                System.out.println("Nama lengkap saya = " + nama);
+                j++;
+            }
+
+        }
+
     }
 }
