c8bfb5348f67748fbf171ebd20cfb68f869cdc10
TeukuReynaldi <91148295+Mystereyn@users.noreply.github.com>
2022-05-16 16:34:24 +0700

    Add files via upload

---
+++ Programmer Zaman Now/src/Looping/Looping/forJava.java
@@ -0,0 +1,12 @@
+package Looping;
+
+public class forJava {
+    public static void main(String[] args) {
+
+        //looping dengna menggunakan variable objek.
+        String nama = "Teuku";
+        for (int i=0; i < 10;  i++) {
+            System.out.println(nama);
+        }
+    }
+}
