22472cc828631fe5f6ad0375c02a4a9be241eabf
Mystereyn <teukureynaldi106@gmail.com>
2022-05-29 13:18:37 +0700

    menambahkan komentar methodReturn

--- Programmer Zaman Now/src/methodJava/methodReturn.java
+++ Programmer Zaman Now/src/methodJava/methodReturn.java
@@ -8,14 +8,17 @@ public class methodReturn { //method return value
             mengembalikan value pada return total lalu masuk ke 
             System.out.println(keseluruhan)  < ini var
         */
+
+        //inisialisasi variable terlebih dahulu
         var value1 = 25;
         var value2 = 50;
-        var keseluruhan = sum(value1,value2);
+        var keseluruhan = tambah(value1,value2);
 
         System.out.println(keseluruhan); // < variable gabungan dari value1 & value2
+
     }
 
-    static int sum(int value1, int value2) {
+    static int tambah(int value1, int value2) {
         var total = value1 + value2;
         return total; 
     }
