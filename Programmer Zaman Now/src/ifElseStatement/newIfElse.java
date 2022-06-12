7e667c5fb6c8ca525d606bcb6cfb07ab5b66e716
Mystereyn <teukureynaldi106@gmail.com>
2022-06-12 18:51:53 +0700

    Add newIfElse.java

---
+++ Programmer Zaman Now/src/ifElseStatement/newIfElse.java
@@ -0,0 +1,25 @@
+package ifElseStatement;
+
+public class newIfElse {
+    public static void main(String[] args) {
+        int hari = 7;
+        if(hari == 1 ) {
+            System.out.println("Senin");
+        } else if(hari == 2) {
+            System.out.println("Selasa");
+        } else if(hari == 4) {
+            System.out.println("Rabu");
+        } else if(hari == 5) {
+            System.out.println("Kamis");
+        } else if(hari == 6) {
+            System.out.println("Jumat");
+        } else if(hari == 7) {
+            System.out.println("Sabtu");
+        } else if(hari == 8) {
+            System.out.println("Minggu");
+        } else {
+            System.out.println("Salah");
+        }
+    }
+    
+}
